3c821db6598b256115e6c94ccc0330fd851aacee
HyoYoungJung <hy8grace@gmail.com>
2024-10-09 23:58:55 +0900

    공연상세정보 수정&해외공연정보 추가

---
+++ src/datas/globalShowInfos.ts
@@ -0,0 +1,38 @@
+export const showInfos = [
+    {
+        id: 1001,
+        name: "FNC BAND KINGDOM 2024 - 일본",
+        address: "",
+        note: [
+            ""
+        ],
+        capacity: "",
+        seats: [
+            ""
+        ],
+        setlist: [
+            "불놀이"
+        ],
+        ootd: [
+            ""
+        ]
+    },
+    {
+        id: 1002,
+        name: "2024 N.Flying LIVE HIDE-OUT IN JAPAN : 요코하마",
+        address: "",
+        note: [
+            ""
+        ],
+        capacity: "",
+        seats: [
+            "",
+        ],
+        setlist: [
+            ""
+        ],
+        ootd: [
+            ""
+        ]
+    },
+]
