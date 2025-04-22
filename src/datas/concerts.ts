fd6bf390be0cda6892f7e056c89bbd35bf6e4e19
HyoYoungJung <hy8grace@gmail.com>
2025-04-22 21:42:03 +0900

    엔콘4 부산 공연 정보 수정

--- src/datas/concerts.ts
+++ src/datas/concerts.ts
@@ -619,8 +619,8 @@ export const concertsData = [
     type: "콘서트",
     performanceType: "단독",
     artists: ["N.Flying"],
-    poster: "/image/show/2025/250705_&CON4_FULL_CIRCLE_BUSAN.jpeg",
-    ticketOpen: { date: "2025-04-21", time: "20:00" }, // todo 일반예매 4/23(수) 업뎃 필요
+    poster: "/image/show/2025/250705_&CON4_FULL_CIRCLE_BUSAN_NF.jpg",
+    ticketOpen: { date: "2025-04-23", time: "20:00" },
     ticketLink: "https://tickets.interpark.com/goods/25005712",
     lat: "35.145093252451666",
     lng: "129.10867582979586",
